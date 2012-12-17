cbde46b3cd5981792969dec631255c4b40e9cb77
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-17 08:56:11 +0000

    Creating a branch.
    git-svn-id: https://plugins.svn.wordpress.org/mw-wp-form/branches/0.5.x@640382 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ form_fields/mw_form_field_tel.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Name: MW Form Field Tel
+ * URI: http://2inc.org
+ * Description: 電話番号フィールドを出力。
+ * Version: 1.0
+ * Author: Takashi Kitajima
+ * Author URI: http://2inc.org
+ * Created: December 14, 2012
+ * License: GPL2
+ *
+ * Copyright 2012 Takashi Kitajima (email : inc@2inc.org)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+class mw_form_field_tel extends mw_form_field {
+
+	/**
+	 * setDefaults
+	 * $this->defaultsを設定し返す
+	 * @return	Array	defaults
+	 */
+	protected function setDefaults() {
+		return array(
+			'name'       => '',
+			'show_error' => 'true',
+		);
+	}
+
+	/**
+	 * inputPage
+	 * 入力ページでのフォーム項目を返す
+	 * @return	String	HTML
+	 */
+	protected function inputPage() {
+		$_ret = $this->Form->tel( $this->atts['name'] );
+		if ( $this->atts['show_error'] !== 'false' )
+			$_ret .= $this->getError( $this->atts['name'] );
+		return $_ret;
+	}
+
+	/**
+	 * previewPage
+	 * 確認ページでのフォーム項目を返す
+	 * @return	String	HTML
+	 */
+	protected function previewPage() {
+		$value = $this->Form->getTelValue( $this->atts['name'] );
+		$_ret  = $value;
+		$_ret .= $this->Form->hidden( $this->atts['name'].'[data]', $value );
+		$_ret .= $this->Form->separator( $this->atts['name'] );
+		return $_ret;
+	}
+}
