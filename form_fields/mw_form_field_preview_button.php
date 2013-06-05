7376f244b747bb6dd8ce4d2ed576eb9b1f3701cb
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-05 16:58:06 +0000

    Version up. 0.8
    git-svn-id: https://plugins.svn.wordpress.org/mw-wp-form/trunk@723183 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form_fields/mw_form_field_preview_button.php
+++ form_fields/mw_form_field_preview_button.php
@@ -3,28 +3,34 @@
  * Name: MW Form Field Preview Button
  * URI: http://2inc.org
  * Description: 確認ボタンを出力。
- * Version: 1.0
+ * Version: 1.1
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Created: December 14, 2012
+ * Modified: May 29, 2013
  * License: GPL2
  *
- * Copyright 2012 Takashi Kitajima (email : inc@2inc.org)
- * 
+ * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
  * published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 class mw_form_field_preview_button extends mw_form_field {
+
+	/**
+	 * String $short_code_name
+	 */
+	protected $short_code_name = 'mwform_previewButton';
 
 	/**
 	 * setDefaults
@@ -33,24 +39,35 @@ class mw_form_field_preview_button extends mw_form_field {
 	 */
 	protected function setDefaults() {
 		return array(
-			'value' => __( 'Confirm', self::DOMAIN ),
+			'value' => __( 'Confirm', MWF_Config::DOMAIN ),
 		);
 	}
 
 	/**
 	 * inputPage
 	 * 入力ページでのフォーム項目を返す
+	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function inputPage() {
-		return $this->Form->submit( $this->Form->getPreviewButtonName(), $this->atts['value'] );
+	protected function inputPage( $atts ) {
+		return $this->Form->submit( $this->Form->getPreviewButtonName(), $atts['value'] );
 	}
 
 	/**
 	 * previewPage
 	 * 確認ページでのフォーム項目を返す
+	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function previewPage() {
+	protected function previewPage( $atts ) {
+	}
+
+	/**
+	 * add_qtags
+	 * QTags.addButton を出力
+	 */
+	protected function add_qtags() {
+	}
+	public function _add_qtags() {
 	}
 }
