0df88ba138b394f9f355060e9ae0bcb04fb4b420
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-24 09:21:39 +0000

    Tagging version 1.3.3
    git-svn-id: https://plugins.svn.wordpress.org/mw-wp-form/tags/1.3.3@863935 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form_fields/mw_form_field_confirm_button.php
+++ form_fields/mw_form_field_confirm_button.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Name: MW Form Field Preview Button
+ * Name: MW Form Field Confirm Button
  * URI: http://2inc.org
  * Description: 確認ボタンを出力。
- * Version: 1.1.3
+ * Version: 1.2.3
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
- * Created: December 14, 2012
- * Modified: August 6, 2013
+ * Created : December 14, 2012
+ * Modified: December 29, 2013
  * License: GPL2
  *
  * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
@@ -25,12 +25,24 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-class mw_form_field_preview_button extends mw_form_field {
+class mw_form_field_confirm_button extends mw_form_field {
 
 	/**
-	 * String $short_code_name
+	 * String $shortcode_name
 	 */
-	protected $short_code_name = 'mwform_previewButton';
+	protected $shortcode_name = 'mwform_confirmButton';
+
+	/**
+	 * __construct
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->set_qtags(
+			$this->shortcode_name,
+			__( 'Confirm Button', MWF_Config::DOMAIN ),
+			$this->shortcode_name
+		);
+	}
 
 	/**
 	 * setDefaults
@@ -46,32 +58,17 @@ class mw_form_field_preview_button extends mw_form_field {
 	/**
 	 * inputPage
 	 * 入力ページでのフォーム項目を返す
-	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function inputPage( $atts ) {
-		return $this->Form->submit( $this->Form->getPreviewButtonName(), $atts['value'] );
+	protected function inputPage() {
+		return $this->Form->submit( $this->Form->getConfirmButtonName(), $this->atts['value'] );
 	}
 
 	/**
-	 * previewPage
+	 * confirmPage
 	 * 確認ページでのフォーム項目を返す
-	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function previewPage( $atts ) {
-	}
-
-	/**
-	 * add_qtags
-	 * QTags.addButton を出力
-	 */
-	protected function add_qtags() {
-		?>
-		'<?php echo $this->short_code_name; ?>',
-		'<?php _e( 'Confirm Button', MWF_Config::DOMAIN ); ?>',
-		'[<?php echo $this->short_code_name; ?>]',
-		''
-		<?php
+	protected function confirmPage() {
 	}
 }
--- form_fields/mw_form_field_hidden.php
+++ form_fields/mw_form_field_hidden.php
@@ -3,11 +3,11 @@
  * Name: MW Form Field Hidden
  * URI: http://2inc.org
  * Description: hiddenフィールドを出力。
- * Version: 1.1
+ * Version: 1.3.4
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
- * Created: December 14, 2012
- * Modified: May 29, 2013
+ * Created : December 14, 2012
+ * Modified: December 29, 2013
  * License: GPL2
  *
  * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
@@ -28,9 +28,21 @@
 class mw_form_field_hidden extends mw_form_field {
 
 	/**
-	 * String $short_code_name
+	 * String $shortcode_name
 	 */
-	protected $short_code_name = 'mwform_hidden';
+	protected $shortcode_name = 'mwform_hidden';
+
+	/**
+	 * __construct
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->set_qtags(
+			$this->shortcode_name,
+			__( 'Hidden', MWF_Config::DOMAIN ),
+			$this->shortcode_name . ' name=""'
+		);
+	}
 
 	/**
 	 * setDefaults
@@ -41,40 +53,34 @@ class mw_form_field_hidden extends mw_form_field {
 		return array(
 			'name'  => '',
 			'value' => '',
+			'echo'  => 'false',
 		);
 	}
 
 	/**
 	 * inputPage
 	 * 入力ページでのフォーム項目を返す
-	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function inputPage( $atts ) {
-		return $this->Form->hidden( $atts['name'], $atts['value'] );
+	protected function inputPage() {
+		$echo_value = '';
+		if ( $this->atts['echo'] === 'true' ) {
+			$echo_value = $this->atts['value'];
+		}
+		return $echo_value . $this->Form->hidden( $this->atts['name'], $this->atts['value'] );
 	}
 
 	/**
-	 * previewPage
+	 * confirmPage
 	 * 確認ページでのフォーム項目を返す
-	 * @param	Array	$atts
 	 * @return	String	HTML
 	 */
-	protected function previewPage( $atts ) {
-		$value = $this->Form->getValue( $atts['name'] );
-		return $this->Form->hidden( $atts['name'], $value );
-	}
-
-	/**
-	 * add_qtags
-	 * QTags.addButton を出力
-	 */
-	protected function add_qtags() {
-		?>
-		'<?php echo $this->short_code_name; ?>',
-		'<?php _e( 'Hidden', MWF_Config::DOMAIN ); ?>',
-		'[<?php echo $this->short_code_name; ?> name=""]',
-		''
-		<?php
+	protected function confirmPage() {
+		$value = $this->Form->getValue( $this->atts['name'] );
+		$echo_value = '';
+		if ( $this->atts['echo'] === 'true' ) {
+			$echo_value = $value;
+		}
+		return $echo_value . $this->Form->hidden( $this->atts['name'], $value );
 	}
 }
